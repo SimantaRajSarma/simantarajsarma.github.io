41d16972b69ba0b8f55732c59288dc4ba9e06ab1
SimantaRajSarma <simantarajsarma@gmail.com>
2023-12-19 00:53:25 +0530

    Back btn functionality added

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
 const container = document.querySelector('div:nth-child(1)');
 
+    const homeBtn = document.querySelector('button:nth-child(5)');
+
         const h2 = document.querySelector('h2');
-        const footer = document.querySelector('footer');
-
         const day = document.querySelector('div:nth-child(2)');
-
         const night = document.querySelector('div:nth-child(3)');
-
         const day_btn = document.querySelector('button:nth-child(2)');
-
         const night_btn = document.querySelector('button:nth-child(3)');
+        const footer = document.querySelector('footer');
+      
+        // Change Page according to button click
+        function changePage(value){        
+            
+                const btnValue = ['Day Shift', 'Night Shift'];
 
-        function changePage(value){                
                 container.classList.remove('day-time', 'night-time', 'container');
     
-                if(value == 'Day Shift'){
+                if(value == btnValue[0]){
                     container.classList.add('day-time');
                     h2.textContent = "I Build Softwares 😎";
                     footer.textContent = 'Build with ❤️';
+                    homeBtn.style.display = 'block';
 
                 }
-                else if(value == 'Night Shift'){
+                else if(value == btnValue[1]){
                     container.classList.add('night-time');
                     h2.textContent = "I Break Softwares😈";
                     footer.textContent = 'Build with 💙';
+                    homeBtn.style.display = 'block';
                 }
                 else{
                     console.log('error');
                 }
         }
         
+        homeBtn.addEventListener('click', home);
+
+        // Return home function
+        function home(){
+            window.history.go();
+        }
+
+        // Listening to btn click
         night_btn.addEventListener('click', function () {
             changePage(this.textContent);
-
         });
-
         day_btn.addEventListener('click', function () {
             changePage(this.textContent);
         });
